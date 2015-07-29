21f35e20af6c6599c80ca811e48ea6fd2414f222
Robert Marianski <rob@marianski.com>
2015-07-29 11:25:23 -0400

    Version bump post release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def test_suite():
     return suite
 
 setup(name='mapbox-vector-tile',
-      version='0.0.11',
+      version='0.0.12.dev0',
       description=u"Mapbox Vector Tile",
       long_description=long_description,
       classifiers=[],
