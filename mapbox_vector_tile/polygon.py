e612122987ac1a30124226347ad2f06343153faf
Thierry Bomandouki <thierry.bomandouki.ext@mappy.com>
2016-10-28 13:50:04 +0000

    Adding extra function which cleans an invalid MultiPolygon

--- mapbox_vector_tile/polygon.py
+++ mapbox_vector_tile/polygon.py
@@ -40,17 +40,18 @@ def _drop_degenerate_inners(shape):
     return Polygon(shape.exterior, new_inners)
 
 
-def _contour_to_poly(contour):
+def _contour_to_poly(contour, asserted):
     poly = Polygon(contour)
     if not poly.is_valid:
         poly = poly.buffer(0)
-    assert poly.is_valid, \
-        "Contour %r did not make valid polygon %s because %s" \
-        % (contour, poly.wkt, explain_validity(poly))
+    if asserted:
+        assert poly.is_valid, \
+            "Contour %r did not make valid polygon %s because %s" \
+            % (contour, poly.wkt, explain_validity(poly))
     return poly
 
 
-def _polytree_node_to_shapely(node):
+def _polytree_node_to_shapely(node, asserted):
     """
     Recurses down a Clipper PolyTree, extracting the results as Shapely
     objects.
@@ -61,7 +62,7 @@ def _polytree_node_to_shapely(node):
     polygons = []
     children = []
     for ch in node.Childs:
-        p, c = _polytree_node_to_shapely(ch)
+        p, c = _polytree_node_to_shapely(ch, asserted)
         polygons.extend(p)
         children.extend(c)
 
@@ -76,9 +77,9 @@ def _polytree_node_to_shapely(node):
             children = []
 
     elif node.Contour:
-        poly = _contour_to_poly(node.Contour)
+        poly = _contour_to_poly(node.Contour, asserted)
         for ch in children:
-            inner = _contour_to_poly(ch)
+            inner = _contour_to_poly(ch, asserted)
             diff = poly.difference(inner)
             if not diff.is_valid:
                 diff = diff.buffer(0)
@@ -97,7 +98,8 @@ def _polytree_node_to_shapely(node):
             if diff.is_valid:
                 poly = diff
 
-        assert poly.is_valid
+        if asserted:
+            assert poly.is_valid
         if poly.type == 'MultiPolygon':
             polygons.extend(poly.geoms)
         else:
@@ -114,19 +116,20 @@ def _polytree_node_to_shapely(node):
     return (polygons, children)
 
 
-def _polytree_to_shapely(tree):
-    polygons, children = _polytree_node_to_shapely(tree)
+def _polytree_to_shapely(tree, asserted):
+    polygons, children = _polytree_node_to_shapely(tree, asserted)
 
     # expect no left over children - should all be incorporated into polygons
     # by the time recursion returns to the root.
     assert len(children) == 0
 
     union = cascaded_union(polygons)
-    assert union.is_valid
+    if asserted:
+        assert union.is_valid
     return union
 
 
-def make_valid_pyclipper(shape):
+def make_valid_pyclipper(shape, asserted):
     """
     Use the pyclipper library to "union" a polygon on its own. This operation
     uses the even-odd rule to determine which points are in the interior of
@@ -153,10 +156,10 @@ def make_valid_pyclipper(shape):
     except pyclipper.ClipperException:
         return MultiPolygon([])
 
-    return _polytree_to_shapely(result)
+    return _polytree_to_shapely(result, asserted)
 
 
-def make_valid_polygon(shape):
+def make_valid_polygon(shape, asserted):
     """
     Make a polygon valid. Polygons can be invalid in many ways, such as
     self-intersection, self-touching and degeneracy. This process attempts to
@@ -174,19 +177,20 @@ def make_valid_polygon(shape):
 
     assert shape.geom_type == 'Polygon'
 
-    shape = make_valid_pyclipper(shape)
-    assert shape.is_valid
+    shape = make_valid_pyclipper(shape, asserted)
+    if asserted: 
+        assert shape.is_valid
     return shape
 
 
-def make_valid_multipolygon(shape):
+def make_valid_multipolygon(shape, asserted):
     new_g = []
 
     for g in shape.geoms:
         if g.is_empty:
             continue
 
-        valid_g = make_valid_polygon(g)
+        valid_g = make_valid_polygon(g, asserted)
 
         if valid_g.type == 'MultiPolygon':
             new_g.extend(valid_g.geoms)
@@ -196,18 +200,108 @@ def make_valid_multipolygon(shape):
     return MultiPolygon(new_g)
 
 
-def make_it_valid(shape):
+def make_it_valid(shape, asserted=True):
     """
     Attempt to make any polygon or multipolygon valid.
     """
-
+    
     if shape.is_empty:
         return shape
 
     elif shape.type == 'MultiPolygon':
-        shape = make_valid_multipolygon(shape)
+        shape = make_valid_multipolygon(shape, asserted)
 
     elif shape.type == 'Polygon':
-        shape = make_valid_polygon(shape)
+        shape = make_valid_polygon(shape, asserted)
 
     return shape
+
+
+def split_multi(shape):
+    """
+    Separate multipolygon into 1 polygon per row
+    """
+    polygons = []
+    pnum = 1 # polygon number
+    for p in shape:
+        polygons.append( (p, pnum) )
+        pnum += 1
+    return polygons 
+
+
+def split_line(polygons):
+    """
+    Break each polygon into linestrings
+    """
+    linestrings = []
+    for p, pnum in polygons:
+        lnum = 0 # line number
+        boundary = p.boundary
+        if boundary.type == 'LineString':
+            linestrings.append( (boundary, pnum, lnum) )
+        else:
+            for ls in boundary:
+                linestrings.append( (ls, pnum, lnum) )
+                lnum += 1
+    return linestrings
+
+
+def line_exterior(linestrings):
+    """
+    Get the linestrings that make up the exterior of each 
+    """
+    exterior_lines = []
+    for l, pnum, lnum in linestrings:
+        if lnum == 0: 
+           exterior_lines.append( (l, pnum) )
+    return exterior_lines
+
+
+def line_interior(linestrings):
+    """
+    Get an array of all the linestrings that make up the
+    interior of each polygon
+    """
+    interior_lines = []
+    for l, pnum, lnum in linestrings:
+        if lnum > 0: 
+           interior_lines.append( (l, pnum) )
+    return interior_lines
+
+
+def get_line_polygon(p_linestrings, p_num):
+    lines = []
+    for l, pnum in p_linestrings:
+        if pnum == p_num:
+            lines.append(l)
+    return lines
+
+
+def poly_geom(exterior_linestrings, interior_linestrings):
+    """
+    Rebuild the polygons
+    """
+    polygons = []
+    for el, pnum in exterior_linestrings :
+        lines = get_line_polygon(interior_linestrings, pnum)
+        if len(lines) == 0 : # no interior line
+            polygons.append(Polygon(el).buffer(0))
+        else:
+            for l in lines:
+                polygons.append(Polygon(el, Polygon(l).interiors).buffer(0)) 
+    return MultiPolygon(polygons)
+
+
+def clean_multi(shape):
+    """
+    Remove self- and ring-selfintersections from input Polygon geometries
+    """
+    polygons = split_multi(shape)
+    linestrings = split_line(polygons)
+    exterior_lines = line_exterior(linestrings)
+    interior_lines = line_interior(linestrings)
+    poly = poly_geom(interior_lines, exterior_lines)
+    assert poly.is_valid, \
+        "Not valid polygon %s because %s" \
+        % (poly.wkt, explain_validity(poly))
+    return poly
