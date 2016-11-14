4bd85b30d7cd68b1fa8635c2d943af4a69916430
Thierry Bomandouki <thierry.bomandouki.ext@mappy.com>
2016-11-14 12:33:30 +0000

    Remove unused import

--- mapbox_vector_tile/encoder.py
+++ mapbox_vector_tile/encoder.py
@@ -1,5 +1,4 @@
 from mapbox_vector_tile.polygon import make_it_valid, clean_multi
-from math import fabs
 from numbers import Number
 from past.builtins import long
 from past.builtins import unicode
