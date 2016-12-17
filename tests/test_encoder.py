a07da5066449b91e49f3f26caa9eabde3dcd8513
Rory McCann <rory@technomancy.org>
2016-12-17 18:06:28 +0100

    correct test

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -340,10 +340,13 @@ class TestDifferentGeomFormats(BaseTestCase):
     def test_encode_multipoint(self):
         geometry = 'MULTIPOINT((10 10), (20 20), (10 40), (40 40), (30 30), (40 20), (30 10))'  # noqa
         self.assertRoundTrip(input_geometry=geometry,
-                             expected_geometry=[
-                                 [10, 10], [20, 20], [10, 40],
-                                 [40, 40], [30, 30], [40, 20], [30, 10],
-                             ])
+                             expected_geometry={
+                                 'type': 'MultiPoint',
+                                 'coordinates': [
+                                     [10, 10], [20, 20], [10, 40], [40, 40],
+                                     [30, 30], [40, 20], [30, 10]
+                                 ]
+                             })
 
     def test_encode_multilinestring(self):
         geometry = 'MULTILINESTRING ((10 10, 20 20, 10 40), (40 40, 30 30, 40 20, 30 10))'  # noqa
